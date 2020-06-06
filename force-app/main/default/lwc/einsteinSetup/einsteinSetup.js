a05154d5e1b90da4bd181da9a7ac168615b319ad
kevanmoothien <kevanmoothien@gmail.com>
2020-06-06 15:55:04 +0400

    first draft of einstein setup with controller

--- force-app/main/default/lwc/einsteinSetup/einsteinSetup.js
+++ force-app/main/default/lwc/einsteinSetup/einsteinSetup.js
@@ -1,10 +1,43 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from 'lwc'
+import saveCredential from '@salesforce/apex/EinsteinSetupController.saveCredential'
+import importCredential from '@salesforce/apex/EinsteinSetupController.importCredential'
 
 export default class EinsteinSetup extends LightningElement {
-    @api setupRecordId
+    cert
+    email
 
-    handleUploadFinished(event) {
-        const uploadedFiles = event.detail.files;
-        alert("No. of files uploaded : " + uploadedFiles.length);
+    connectedCallback() {
+        importCredential().then((res)=>{
+            console.log(res)
+            this.email = res.email
+        })
+    }
+
+    save() {
+        console.log('omodmeodemode')
+        const email = this.template.querySelector('.input-email').value
+        saveCredential({ email: email, cert: this.cert }).then((res)=>{
+            console.log(res)
+        })
+    }
+
+    handleFileChange(event) {
+        const file = event.detail.files[0]
+        this.processCertificateFile(file)
+    }
+
+    fileReader(file, callback) {
+        let reader = new FileReader()
+        reader.onload = ()=>{
+            let fileContents = reader.result
+            callback(null, fileContents)
+        }
+        reader.readAsText(file)
+    }
+
+    processCertificateFile(file) {
+        this.fileReader(file, (error, result)=>{
+            this.cert = result.replace(/\r\n/g, '|')
+        })
     }
 }
