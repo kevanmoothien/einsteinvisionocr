3ab9a0225abb094ad865d80ee6aea21ed29fa8ac
kevanmoothien <kevanmoothien@gmail.com>
2020-10-29 22:05:53 +0400

    tabular prediction

--- force-app/main/default/lwc/einsteinOCR/einsteinOCR.js
+++ force-app/main/default/lwc/einsteinOCR/einsteinOCR.js
@@ -1,13 +1,24 @@
 import { LightningElement } from 'lwc'
 import scanBusinessCard from '@salesforce/apex/EinsteinController.scanBusinessCard'
+import scanTable from '@salesforce/apex/EinsteinController.scanTable'
 
 export default class EinsteinOCR extends LightningElement {
     imageUrl
     scanResult
     scan() {
         this.scanResult = null
-        this.imageUrl = this.template.querySelector('.input-image-url').value
+        this.imageUrl = this.template.querySelector('.input-business-url').value
         scanBusinessCard({ imageUrl: this.imageUrl }).then((res)=>{
+            if (res != null) {
+                this.scanResult = res;
+            }
+        })
+    }
+
+    scanTable() {
+        this.scanResult = null
+        this.imageUrl = this.template.querySelector('.input-table-url').value
+        scanTable({ imageUrl: this.imageUrl }).then((res)=>{
             if (res != null) {
                 this.scanResult = res;
             }
